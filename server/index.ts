5e30d09cc34cb32244fc23ab1cd581ab7bc241e9
Peter Bondad <mapeebun@gmail.com>
2023-12-04 21:46:03 +0800

    connect to mongoDB

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 
-app.listen(3000, () => console.log("Server running"));
+const mongoURL = process.env.MONGO_URL || "mongodb://localhost:27017/default";
+const port = process.env.PORT || 3000;
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () => console.log("Server running"));
